cbf3aa653c6f2dc2089ba0bc8e2a821c03cc2060
shadow1163 <674602286@qq.com>
2016-10-04 13:56:32 +0800

    write all process' log to log file

--- fork.pl
+++ fork.pl
@@ -1,6 +1,5 @@
 #! /usr/bin/env perl
 
-
 use strict;
 use warnings;
 
@@ -9,8 +8,12 @@ use Fcntl qw(:DEFAULT :flock);
 print("Starting main program\n");
 
 my @childs;
+my @logTmp;
 my $max_process = 5;
 our $logfh;
+
+open($logfh, "> /tmp/fork_log.txt") or die("000000open file error\n");
+close($logfh);
 
 for (my $count = 1; $count <= $max_process; $count++) {
     my $pid = fork();
@@ -30,6 +33,30 @@ for (my $count = 1; $count <= $max_process; $count++) {
 foreach (@childs) {
     my $tmp = waitpid($_,0);
     print("done with pid $tmp\n");
+    if (-e "/tmp/fork_log_$tmp.txt") {
+        print("find file /tmp/fork_log_$tmp.txt\n");
+        push(@logTmp, $tmp);
+    }
+}
+
+while (@logTmp) {
+    my $backfh;
+    my $tmp = shift @logTmp;
+    open($logfh, ">> /tmp/fork_log.txt") or die("222222open file error\n");
+    unless (flock($logfh, LOCK_EX | LOCK_NB)) {
+        close($logfh);
+        push(@logTmp, $tmp);
+        print("log file is locked, next...\n");
+        next;
+    } else {
+        open($backfh, "< /tmp/fork_log_$tmp.txt") or die("11111111open file error\n $!\n");
+        while(<$backfh>) {
+            print $logfh ("$_");
+        }
+    }
+    close($logfh);
+    close($backfh);
+    `rm -rf /tmp/fork_log_$tmp.txt`;
 }
 
 print("End of main program\n");
@@ -44,10 +71,12 @@ sub sub1 {
         $fh = $logfh;
     }
     select($fh);
+    $| = 1;
     print("started threads $num\n");
     sleep($num);
     print("done with threads $num\n");
     close($fh);
+    close($logfh);
     $logfh = undef;
     return($num);
 }
